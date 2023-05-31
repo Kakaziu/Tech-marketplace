7fe0f2b71daff5062c0b52c2a07c4dfe6fba8460
Kauã Ferreira Borba <kaua.borba03@gmail.com>
2023-05-31 14:56:13 -0300

    add choose category

--- src/components/StoreHome/index.jsx
+++ src/components/StoreHome/index.jsx
@@ -18,6 +18,8 @@ const StoreHome = () => {
   const { addProductsInCar } = useContext(ProductsContext);
   const [moveUp, setMoveUp] = useState(false);
   const [linePlace, setLinePlace] = useState("0%");
+  const [chooseCategory, setChooseCategory] = useState(null);
+  const [filteredProducts, setFilteredProducts] = useState([]);
 
   document.addEventListener("scroll", () => {
     if (window.scrollY > 1850) {
@@ -25,7 +27,11 @@ const StoreHome = () => {
     }
   });
 
-  function moveLine(move) {
+  function changeCategory(move, category) {
+    const filter = products.filter((product) => product.category === category);
+
+    setChooseCategory(category);
+    setFilteredProducts(filter);
     setLinePlace(move);
   }
 
@@ -34,11 +40,19 @@ const StoreHome = () => {
       <MoveUp moveUp={moveUp}>
         <MenuProducts>
           <ul>
-            <li onClick={() => moveLine("0%")}>Todos</li>
-            <li onClick={() => moveLine("20%")}>Computadores</li>
-            <li onClick={() => moveLine("41%")}>Celulares</li>
-            <li onClick={() => moveLine("61%")}>Notebooks</li>
-            <li onClick={() => moveLine("83%")}>Periféricos</li>
+            <li onClick={() => changeCategory("0%", null)}>Todos</li>
+            <li onClick={() => changeCategory("20%", "computadores")}>
+              Computadores
+            </li>
+            <li onClick={() => changeCategory("41%", "celulares")}>
+              Celulares
+            </li>
+            <li onClick={() => changeCategory("61%", "notebooks")}>
+              Notebooks
+            </li>
+            <li onClick={() => changeCategory("83%", "periféricos")}>
+              Periféricos
+            </li>
           </ul>
           <div>
             <LineOutside id="line-outside" />
@@ -47,26 +61,47 @@ const StoreHome = () => {
         </MenuProducts>
 
         <Products>
-          {products.map((product) => {
-            return (
-              <Product key={product.id}>
-                <img src={product.urlImg} />
-                <InfoProducts>
-                  <div>
-                    <h3>{product.name}</h3>
-                    <p>{product.description}</p>
-                  </div>
+          {!chooseCategory
+            ? products.map((product) => {
+                return (
+                  <Product key={product.id}>
+                    <img src={product.urlImg} />
+                    <InfoProducts>
+                      <div>
+                        <h3>{product.name}</h3>
+                        <p>{product.description}</p>
+                      </div>
 
-                  <span>
-                    {formatPrice(product.price)}{" "}
-                    <button onClick={() => addProductsInCar(product.id)}>
-                      Adicionar
-                    </button>
-                  </span>
-                </InfoProducts>
-              </Product>
-            );
-          })}
+                      <span>
+                        {formatPrice(product.price)}{" "}
+                        <button onClick={() => addProductsInCar(product.id)}>
+                          Adicionar
+                        </button>
+                      </span>
+                    </InfoProducts>
+                  </Product>
+                );
+              })
+            : filteredProducts.map((product) => {
+                return (
+                  <Product key={product.id}>
+                    <img src={product.urlImg} />
+                    <InfoProducts>
+                      <div>
+                        <h3>{product.name}</h3>
+                        <p>{product.description}</p>
+                      </div>
+
+                      <span>
+                        {formatPrice(product.price)}{" "}
+                        <button onClick={() => addProductsInCar(product.id)}>
+                          Adicionar
+                        </button>
+                      </span>
+                    </InfoProducts>
+                  </Product>
+                );
+              })}
         </Products>
 
         <DirectButton>Ver mais</DirectButton>
