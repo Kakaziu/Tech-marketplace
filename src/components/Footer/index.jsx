265502a6627569503396a57620fd6c9cb3f1098b
Kauã Ferreira Borba <kaua.borba03@gmail.com>
2023-06-19 23:12:25 -0300

    footer mobile

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,12 +1,11 @@
-import { FooterTag ,ContainerFooter, TitleFooter, Finally } from './styled'
-import { FaUserFriends } from 'react-icons/fa'
-import { AiFillCar, AiFillMail, AiFillCreditCard, AiFillHeart } from 'react-icons/ai'
-import { MdPrivacyTip } from 'react-icons/md'
-import {BsTelephoneFill, BsFillTicketFill, BsPhoneFill} from 'react-icons/bs'
+import { FooterTag, ContainerFooter, TitleFooter } from "./styled";
+import { FaUserFriends } from "react-icons/fa";
+import { AiFillCar, AiFillMail, AiFillCreditCard } from "react-icons/ai";
+import { MdPrivacyTip } from "react-icons/md";
+import { BsTelephoneFill, BsFillTicketFill, BsPhoneFill } from "react-icons/bs";
 
-const Footer = () =>{
-
-  return(
+const Footer = () => {
+  return (
     <FooterTag>
       <div>
         <TitleFooter>
@@ -16,38 +15,77 @@ const Footer = () =>{
             trigger="loop"
             delay="2500"
             colors="primary:#30c9e8"
-            style={{width:'40px',height:'40px'}}>
-          </lord-icon>  
+            style={{ width: "40px", height: "40px" }}
+          ></lord-icon>
         </TitleFooter>
       </div>
       <ContainerFooter>
         <div>
           <h3>Institucional</h3>
           <ul>
-            <li><span><FaUserFriends size='22'/></span> Quem somos</li>
-            <li><span><AiFillCar size='22'/></span>Entregas e prazos</li>
-            <li><span><MdPrivacyTip size='22'/></span>Política de privacidade</li>
+            <li>
+              <span>
+                <FaUserFriends size="22" />
+              </span>{" "}
+              Quem somos
+            </li>
+            <li>
+              <span>
+                <AiFillCar size="22" />
+              </span>
+              Entregas e prazos
+            </li>
+            <li>
+              <span>
+                <MdPrivacyTip size="22" />
+              </span>
+              Política de privacidade
+            </li>
           </ul>
         </div>
         <div>
           <h3>Contato</h3>
           <ul>
-            <li><span><BsTelephoneFill size='22'/></span>(21) 99992-8635</li>
-            <li><span><AiFillMail size='22'/></span>techmoos@suporte.com</li>
+            <li>
+              <span>
+                <BsTelephoneFill size="22" />
+              </span>
+              (21) 99992-8635
+            </li>
+            <li>
+              <span>
+                <AiFillMail size="22" />
+              </span>
+              techmoos@suporte.com
+            </li>
           </ul>
         </div>
         <div>
           <h3>Pagamento</h3>
           <ul>
-            <li><span><AiFillCreditCard size='22'/></span>Cartão de crédito e débito</li>
-            <li><span><BsFillTicketFill size='22'/></span>Boleto</li>
-            <li><span><BsPhoneFill size='22'/></span>Pix</li>
+            <li>
+              <span>
+                <AiFillCreditCard size="22" />
+              </span>
+              Cartão de crédito e débito
+            </li>
+            <li>
+              <span>
+                <BsFillTicketFill size="22" />
+              </span>
+              Boleto
+            </li>
+            <li>
+              <span>
+                <BsPhoneFill size="22" />
+              </span>
+              Pix
+            </li>
           </ul>
         </div>
       </ContainerFooter>
-      <Finally>Desenvolvido por Kauã {': )'}</Finally>
     </FooterTag>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- src/components/Footer/styled.js
+++ src/components/Footer/styled.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 export const FooterTag = styled.footer`
   background-color: #0b0b0b;
@@ -6,59 +6,64 @@ export const FooterTag = styled.footer`
   border-top-left-radius: 100px;
   margin-top: 50px;
   color: white;
+  padding: 30px;
 
-  div{
+  div {
     padding-top: 30px;
     margin: auto;
   }
-`
+`;
 
 export const TitleFooter = styled.span`
-    font-size: 25px;
-    color: white;
-    display: flex;
-    width: 13%;
-    font-weight: bold;
-    align-items: center;
-    justify-content: space-between;
-    margin: auto;
-`
+  font-size: 25px;
+  color: white;
+  display: flex;
+  width: 13%;
+  font-weight: bold;
+  align-items: center;
+  justify-content: space-between;
+  margin: auto;
+`;
 
 export const ContainerFooter = styled.div`
   display: flex;
   width: 90%;
   margin: auto;
 
-  ul{
+  ul {
     list-style: none;
     margin-top: 20px;
 
-    li{
+    li {
       display: flex;
       align-items: center;
       margin-top: 10px;
 
-      span{
+      span {
         margin-right: 10px;
         margin-top: 3px;
+      }
+
+      @media (max-width: 850px) {
+        justify-content: center;
       }
     }
   }
 
-  div{
+  div {
     display: flex;
     flex-direction: column;
     align-items: flex-start;
     height: 200px;
-  }
-`
 
-export const Finally = styled.span`
-  width: 100%;
-  display: block;
-  width: 100%;
-  background-color: black;
-  text-align: center;
-  padding: 10px;
-  margin-top: 20px;
-`
+    @media (max-width: 850px) {
+      width: 100%;
+      justify-content: center;
+      align-items: center;
+    }
+  }
+
+  @media (max-width: 850px) {
+    flex-direction: column;
+  }
+`;
